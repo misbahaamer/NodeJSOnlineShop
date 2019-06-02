5d9851b1c5c8ce3fa6df69162621c1cdd63a19a9
Misbah Aamer <misbahuddin.aamer@gmail.com>
2019-06-02 14:41:35 -0400

    express.js

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
-const http = require('http');
+const http    = require('http');
+const express = require('express')
+const app     = express()
 
-const routes = require('./route');
+app.use('/users', (req, res, next) => {
+    console.log('in the middleware')
+    res.send('<h1>bismillah hir rahmaanirraheem</h1>');
+})
+app.use('/', (req, res, next) => {
+    console.log('in another middleware')
+    res.send('<h1>bismillah</h1>');
+})
 
-const server = http.createServer(routes.handler);
-server.listen(5555);
+app.listen(5555)
