fb3810dc22025d7f5f113ac85e7bbd9ab7d0fbf5
Misbah Aamer <misbahuddin.aamer@gmail.com>
2020-10-28 20:24:06 -0400

    404 page

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-const express     = require('express')
-const app         = express()
-const bodyParser  = require('body-parser')
+const express     = require('express');
+const app         = express();
+const bodyParser  = require('body-parser');
 
-const adminRoutes = require('./routes/admin.js')
-const shopRoutes  = require('./routes/shop.js')
+const adminRoutes = require('./routes/admin.js');
+const shopRoutes  = require('./routes/shop.js');
 
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(shopRoutes)
-app.use(adminRoutes)
+app.use(shopRoutes);
+app.use(adminRoutes);
 
+app.use((req, res, next) => {
+    res.status(404).send('<h1>page not found</h1>');
+});
 
-
-app.listen(5555)
+app.listen(5555);
